c03e88e68eb4f5431ae34b3ebcf187afc67c97bb
Mohammed Arbaz <mdarbaz228@gmail.com>
2025-08-17 17:30:59 +0530

    Modified few lines

--- src/test/java/com/OrderArt/Functionality/Implementation/Login_Updated.java
+++ src/test/java/com/OrderArt/Functionality/Implementation/Login_Updated.java
@@ -3,6 +3,7 @@ package com.OrderArt.Functionality.Implementation;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import com.OrderArt.WebPages.Login_Landing_Page;
 import com.OrderArt.WebPages.Updated_Login_Page;
 
 public class Login_Updated {
@@ -15,12 +16,15 @@ public class Login_Updated {
 			cd.get("https://front1.orderart.com.au/home/index");
 			//Login_User l = new Login_User();
 			Updated_Login_Page ulp = new Updated_Login_Page(cd);
+			Login_Landing_Page llp = new Login_Landing_Page(cd);
 			ulp.Click_LoginButton();
 			//ulp.Wait_Modal();
 			ulp.Fill_UserName();
 			ulp.Fill_Password();
 			ulp.Click_Consent_CheckBox();
 			ulp.Click_SignInButton();
+			llp.Wait_Span();
+			llp.Get_User_Details_Span();
 			
 		} catch(Throwable t) {
 			System.out.println(t.getMessage());
---
+++ src/test/java/com/OrderArt/WebPages/Login_Landing_Page.java
@@ -0,0 +1,35 @@
+package com.OrderArt.WebPages;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.OrderArtTesting.utilities.AssertUtil;
+
+public class Login_Landing_Page {
+
+	ChromeDriver cd;
+	WebDriverWait w;
+	static String name_of_user;
+	boolean login_flag;
+	
+	public Login_Landing_Page(ChromeDriver cd) {
+		this.cd = cd;
+	}
+	
+	By User_Details_Span = By.xpath("/html/body/header/div[1]/div/div[2]/ul/li[1]/a/span");
+	
+	public void Wait_Span() {
+		WebDriverWait wait = new WebDriverWait(cd,Duration.ofSeconds(15));
+		wait.until(ExpectedConditions.visibilityOf(cd.findElement(By.xpath("/html/body/header/div[1]/div/div[2]/ul/li[1]/a/span"))));
+	}
+	public void Get_User_Details_Span() {
+		name_of_user = cd.findElement(User_Details_Span).getText();
+		AssertUtil.Assert_ValidateEquals("Hello Mohammed", name_of_user, "User Details Matched");
+		login_flag= true;
+		System.out.println("Welcome"+name_of_user);
+	}
+}
